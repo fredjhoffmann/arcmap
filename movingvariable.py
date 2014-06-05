2ffd6c2d68e6d62c61dd0c53521f7950599ce9e7
fredjhoffmann <fredjhoffmann@gmail.com>
2014-06-05 15:53:24 -0400

    Update movingvariable.py

--- movingvariable.py
+++ movingvariable.py
@@ -17,7 +17,21 @@ print(v) ##essentially prints the same list of numbers as IN_FID, but to be used
 
 #find an expression where n will be a revolving value in u, same for m in v
 
-n={0}.format(u)
-m={0}.format(v)
+n=
+m=
 
 #n and m will then be run in a CostPath analysis, with n as the ID of the source poligon and m that of the destination.
+costpath=arcpy.sa.CostPath('n' in 'miyun_villages_poly', 'costD', 'costB','EACH_ZONE','m' in 'miyun_villages_poly')
+##find proper synthax for n to make sure it keeps the spatial data related to it in miyun villages poly. 
+cursor=arcpy.da.InsertCursor('output','n','m')
+
+threshhold_cost=1000
+while (n<=4909):
+	if n == m:
+		skip
+	if 'costpath'<='threshhold_cost':
+		cursor.insertRow([n],[m])
+		del cursor
+	else:
+ 		print "done"
+ 		n+=1
