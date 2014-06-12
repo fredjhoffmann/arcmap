1a7ddd1f0bb97b0877aecccf200109171d48801e
fredjhoffmann <fredjhoffmann@gmail.com>
2014-06-12 11:08:39 -0400

    Update extractandcostdist.py

--- extractandcostdist.py
+++ extractandcostdist.py
@@ -18,7 +18,7 @@ def myfun(row, start, end):
 		arcpy.sa.CostDistance(out_fl,cost,100000).save('codist_{0}'.format(FID))
 		arcpy.Delete_management(out_fl)
 		t2 = time.time()
-		print 'done_%s: time taken %0.3f ms: %i remain' % FID, ((t2-t1)*1000), (end - int(FID))
+		print 'done_{0}: time taken {1} ms:{2} rows remain'.format(FID, ((t2-t1)*1000), (end - int(FID))
 
 fc='C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\Miyun_everything_raw\\popcenters\\miyun_villages_poly.shp'
 cursor=arcpy.SearchCursor(fc)
