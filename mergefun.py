9b8b8981b4eb3f78ecc44708f55032b95ea0fbf3
fredjhoffmann <fredjhoffmann@gmail.com>
2014-06-13 12:19:49 -0400

    Update mergefun.py

--- mergefun.py
+++ mergefun.py
@@ -1,25 +1,23 @@
-import arcpy
-arcpy.env.workspace='C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\miyun_allvillages.gdb'
-import arcpy.sa
-from arcpy.sa import CostPath
-from arcpy.sa import CostDistance
-import arcpy.da
-cost='C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\settlements_draft.gdb\\costlayer'
-
 def mergefun(row, start, end):
 	FIDa=str(row.getValue("FID"))
 	where = '"FID" =' + FIDa
 	out_fla='out_fl{0}'.format(FIDa)
-	for FIDa in range (start,finish): 
+	FIDb=str(row.getValue("FID"))
+	out_flab='out_fl{0}'.format(FIDb)
+	for FIDa in range (start,end): 
 		arcpy.MakeFeatureLayer_management(fc,out_fla,where) #i want acodist_{0} to cycle through all the codist layers, while out_fl stays the same
-		FIDb=str(row.getValue("FID"))
-		out_flab='out_fl{0}'.format(FIDb)
-		for FIDb in range (start, finish):
-			arcpy.sa.ExtractByMask('codist_{0}'.format(FIDb), out_fla).save(extract)
-		if arcpy.GetRasterProperties_management(extract, "MINIMUM") <= threshold:
-			arcpy.Merge_management([out_flab, out_fla], '"joined_{0}".format(FIDb) "and" {0}.format(FIDa)') ##the format() should be the number of the codist_{0} which passed the condition, so that the out_fl is the one which is considered joinable 
-		arcpy.Delete_management(FIDa)
-		arcpy.Delete_management(FIDb)
+		for FIDb in range (start, end):
+			arcpy.CalculateStatistics_management([arcpy.sa.ExtractByMask('C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\miyun_allvillages.gdb\\codist_{0}'.format(FIDb), out_fla)]).save('extractstat{0}'.format(FIDb))
+			t1=time.asctime()
+			print 'codist_{0}_extracted, time completed {1}'.format(FIDb, t1)
+			if arcpy.GetRasterProperties_management('extractstat{0}'.format(FIDb), "MINIMUM") <= threshold:
+				arcpy.Merge_management([out_flab, out_fla], 'C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\joinedtowns.gdb\\"merged_{0}".format(FIDb) "and" {0}.format(FIDa)') ##the format() should be the number of the codist_{0} which passed the condition, so that the out_fl is the one which is considered joinable 
+				t2=time.asctime()
+				print 'merged{0}{1}, time completed {2}'.format(FIDa,FIDb,t2)
+	arcpy.Delete_management(FIDa)
+	arcpy.Delete_management(FIDb)
+	arcpy.Delete_management('C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\joinedtowns.gdb\\extract{0}'.format(FIDb))
+	arcpy.Delete_management('C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\joinedtowns.gdb\\extractstat{0}'.format(FIDb))
 
 
 
@@ -27,4 +25,4 @@ fc='C:\\Users\\frederichoffmann\\Desktop\\ESRI_summer2014\\Miyun_everything_raw\
 cursor=arcpy.SearchCursor(fc)
 for row in cursor:
 	threshold=50000
-	mergefun(row, 0, 2)
+	mergefun(row, 0, 5)
